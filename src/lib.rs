15dfb8ee79ccb652ad560a1c40f04e4c403a1e85
Aryan Malik <aryanhunny98@gmail.com>
2024-02-13 23:24:55 +0530

    Added Contract with tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![no_std]
+use soroban_sdk::{contract, contractimpl, symbol_short, vec, Env, Symbol, Vec};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[contract]
+pub struct Contract;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[contractimpl]
+impl Contract {
+    /// Say Hello to someone or something.
+    /// Returns a length-2 vector/array containing 'Hello' and then the value passed as `to`.
+    pub fn hello(env: Env, to: Symbol) -> Vec<Symbol> {
+        vec![&env, symbol_short!("Hello"), to]
     }
 }
+#[cfg(test)]
+mod test;
