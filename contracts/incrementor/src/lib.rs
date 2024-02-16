d8afd6ab4c9ecd265cf4091ef9db3733b139aeff
Aryan Malik <aryanhunny98@gmail.com>
2024-02-16 17:53:05 +0530

    Updated Incrementor Contract

--- contracts/incrementor/src/lib.rs
+++ contracts/incrementor/src/lib.rs
@@ -22,6 +22,33 @@ impl IncrementorContract {
 
         count
     }
+
+    pub fn get_current_value(env: Env) -> u32 {
+        let count: u32 = env.storage().instance().get(&COUNTER).unwrap_or(0);
+        count
+    }
+
+    pub fn decrement(env: Env) -> u32 {
+        let mut count: u32 = env.storage().instance().get(&COUNTER).unwrap_or(0);
+
+        count -= 1;
+
+        log!(&env, "count: {}", count);
+
+        env.storage().instance().set(&COUNTER, &count);
+
+        env.storage().instance().extend_ttl(100, 100);
+
+        count
+    }
+
+    pub fn reset(env: Env) -> u32 {
+        let count: u32 = 0;
+
+        env.storage().instance().set(&COUNTER, &count);
+
+        count
+    }
 }
 
 #[cfg(test)]
